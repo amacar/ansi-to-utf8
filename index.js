9de367e6d637d7dc6fa032c3db3ffd597cea9fa7
Amadej Pevec <amadej.pevec@gmail.com>
2021-10-28 17:23:45 +0200

    feat: add option to use package as global

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ const { resolve } = require("path");
 const iconv = require("iconv-lite");
 const isUtf8 = require("is-utf8");
 
-const dirName = "./convert";
-const fileNames = readdirSync(dirName);
-
 const isBom = (file) => file[0] === 0xef && file[1] === 0xbb && file[2] === 0xbf;
 
-fileNames.forEach((fileName) => {
-  const path = resolve(dirName, fileName);
-  if (!path.endsWith(".srt")) return;
+module.exports = () => {
+  const dir = process.cwd();
+  const fileNames = readdirSync(dir);
+  fileNames.forEach((fileName) => {
+    const path = resolve(dir, fileName);
+    if (!path.endsWith(".srt")) return;
 
-  chmodSync(path, 0o666);
-  const file = readFileSync(path);
-  const isFileUtf8 = isUtf8(file);
+    chmodSync(path, 0o666);
+    const file = readFileSync(path);
+    const isFileUtf8 = isUtf8(file);
 
-  if (isFileUtf8 && isBom(file)) return;
+    if (isFileUtf8 && isBom(file)) return;
 
-  const str = iconv.decode(file, isFileUtf8 ? "utf8" : "win1250");
-  const buf = iconv.encode(str, "utf8", { addBOM: true });
-  writeFileSync(path, buf);
-});
+    const str = iconv.decode(file, isFileUtf8 ? "utf8" : "win1250");
+    const buf = iconv.encode(str, "utf8", { addBOM: true });
+    writeFileSync(path, buf);
+    console.log(`converted: ${path}`);
+  });
+};
